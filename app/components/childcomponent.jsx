208c0aa2c4b4bba24fe8ab3fae8eed6bf6a386e6
Andriy Fedorchenko <doksel7@gmail.com>
2018-03-02 10:49:48 +0200

    create project jstesttask

---
+++ app/components/childcomponent.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+ 
+class ChildComponent extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = { };
+                           
+        this.func = this.func.bind(this);
+    }
+    func(){
+
+    }
+               
+    render() {
+        return(
+            <div>         
+
+            </div>);
+    }
+}
+ 
+export default ChildComponent;
